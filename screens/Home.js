f7d487f0eee31288829fe0acd0c91068e7915f5e
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2020-05-20 20:20:31 +0530

    Worked with design

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,25 @@
 import React,{Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 export default class Home extends Component{
+  constructor(props){
+      super(props);
+      this.state={
+          title:'PhotoClicker',
+          photo:''
+          
+      }
+  }
+
   render(){
+  let photo=(this.state.photo||'');  
+
   return (
     <View style={styles.container}>
-      <Text>Welcome to Home</Text>
+      <Image resizeMode='center' style={styles.imageHolder} 
+      source={(photo==='')?require('../assets/email.png'):photo}/>
+      <Button title='Take Photo' style={styles.button} 
+      onPress={()=>this.props.navigation.navigate('Camera')}/>
     </View>
   );
 }
@@ -17,4 +31,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageHolder:{
+      alignSelf:'center'
+  },
+  button:{
+    margin:20
+  }
 });
